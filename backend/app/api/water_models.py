4502274998fdf45cb5fe920f5fdda4d7b9bd0bf1
Timurkafed <federror.tim@gmail.com>
2026-01-27 23:49:47 +0500

    добавил метод поиска по фромуле

--- backend/app/api/water_models.py
+++ backend/app/api/water_models.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
 from app.db import get_db
-from app.models import WaterObject
+from app.models import WaterObject, WaterQuality
+from app.schemas.water_schemas import QualityFilter
 from app.api.auth import get_current_user
 
 router = APIRouter(
@@ -34,3 +35,28 @@ def search_lakes(
         .filter(WaterObject.name.ilike(f"%{q}%"))
         .all()
     )
+
+
+@router.post("/search-by-quality")
+def search_by_quality(
+    filter: QualityFilter,
+    current_user=Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    if current_user.get("sub") != "admin":
+        raise HTTPException(status_code=403, detail="Forbidden")
+    
+    # Начинаем с базовой таблицы WaterObject и присоединяем качество воды
+    query = db.query(WaterObject).join(WaterQuality)
+    
+    # Проходим по каждому параметру в фильтре
+    for param_name, param_value in filter.model_dump(exclude_none=True).items():
+        column = getattr(WaterQuality, param_name)
+        
+        if isinstance(param_value, list):  # [5, 10] = диапазон
+            query = query.filter(column >= param_value[0])
+            query = query.filter(column <= param_value[1])
+        else:  # 5 = точное значение
+            query = query.filter(column == param_value)
+    
+    return query.all()
